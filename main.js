c39dcca86bb9e2797c521e940f1506753aae8c17
Sebastian Garatti <garatti.sebastian@gmail.com>
2020-03-16 15:35:43 +0100

    sexto commit

--- main.js
+++ main.js
@@ -11,13 +11,25 @@ function buscar(){
         url: `https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=en-EN&query=${search}}`
     }).then(res => {
         let movies = res.data.results;
-        for (let i = 0;  i< movies.length; i++) {
-            console.log(urlImagen,movies[i].poster_path);
+        for (var i = 0;  i< movies.length; i++) {
+
+            console.log(movies,urlImagen,movies[i].poster_path);
             filmsToSearch += `<img src="${urlImagen}${movies[i].poster_path}">
         <p class="title">${movies[i].title}</p>
         <p class="overview">${movies[i].overview}</p>
         <p>${movies[i].popularity}</p>`;
         }
-        document.getElementById('titulo').innerHTML = filmsToSearch;
+        console.log(Math.round((movies.length)/3),Math.round((movies.length)*2/3))
+        if(i < ((Math.round(movies.length))/3)){
+            document.getElementById('one').innerHTML = filmsToSearch;
+        }
+        // if (i>(Math.round((movies.length)*2/3))){
+        //     document.getElementById('three').innerHTML = filmsToSearch;
+        // }
+        else{
+            document.getElementById('two').innerHTML = filmsToSearch;
+        }
+
+        
    })
 }
